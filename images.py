601d9f715ec202db19d47f75fce38015f95a155e
Ilona_Laskowska <ilona.laskowska@aifi.com>
2025-04-26 00:20:22 +0200

    fix: img path

--- images.py
+++ images.py
@@ -3,7 +3,7 @@ import base64
 from openai import OpenAI
 
 
-def generate_image(prompt):
+def generate_image(prompt, output_path):
     result = client.images.generate(
         model="gpt-image-1",
         prompt=prompt,
@@ -12,7 +12,7 @@ def generate_image(prompt):
     )
     image_base64 = result.data[0].b64_json
     image_bytes = base64.b64decode(image_base64)
-    with open("sample_images/img_0.png", "wb") as f:
+    with open(output_path, "wb") as f:
         f.write(image_bytes)
 
 
@@ -39,6 +39,6 @@ if __name__ == "__main__":
     fairy_tale_description = "A little girl from the reference image meets a new friend from another galaxy. She remains the same but in a different pose."
 
     # Generate the first image based on the main character description
-    image_response = generate_image(main_character_description)
+    image_response = generate_image(main_character_description, output_path="sample_images/img_0.png")
     image_path = "sample_images/img_0.png"
     image_response = generate_image_from_img(fairy_tale_description, image_path, output_path="sample_images/img_1.png")
