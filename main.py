beb60d21cddf46287efb8239cb27c4b5d2789afa
Krzysztof Sakowski <KrzysztofSakowski@users.noreply.github.com>
2025-04-26 10:05:26 +0200

    Commit

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import asyncio
+
 import uvicorn
 
+from main_agent import main_agent
+from settings import RUN_WITHOUT_VOICE
+
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="localhost", port=8000, reload=True)
+    if not RUN_WITHOUT_VOICE:
+        uvicorn.run("api:app", host="localhost", port=8000, reload=True)
+    else:
+        asyncio.run(main_agent(convo_id="test_convo_id"))
